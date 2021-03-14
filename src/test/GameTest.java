14543bae4eac1fa0ad976f1d7c379d56c9df0ab1
Ana Jimenez <anajimenez.tw@packlink.com>
2021-03-14 19:18:11 +0100

    2 strikes

--- src/test/GameTest.java
+++ src/test/GameTest.java
@@ -51,4 +51,19 @@ public class GameTest {
         game.roll(3);
         assertEquals(game.score(), 10+5+5+3);
     }
+
+    @Test
+    public void gameWith2Strikes(){
+        Game game = new Game();
+        game.roll(1);
+        game.roll(0);
+        game.roll(8);
+        game.roll(2);
+        game.roll(5);
+        game.roll(3);
+        game.roll(7);
+        game.roll(3);
+        game.roll(1);
+        assertEquals(game.score(), 1+0+8+2+(5)+5+3+7+3+(1)+1);
+    }
 }
